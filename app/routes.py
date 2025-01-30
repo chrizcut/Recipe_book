c7a6f4f5cd42cdcb660ec389f8395973a040c5a3
chrizcut <christine_cutting@msn.com>
2025-01-30 13:05:08 +0100

    Started with databases

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,23 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from app import app
 from app.forms import ContactForm
 import csv
 
 
 @app.route("/")
-@app.route("/index.html")
-def home():
+@app.route("/index/")
+def index():
     return render_template("index.html")
 
 
-@app.route("/contact_me.html", methods=["GET", "POST"])
+@app.route("/contact_me", methods=["GET", "POST"])
 def contact_me():
     form = ContactForm()
     if form.validate_on_submit():
-        return redirect("form_submitted.html")
-    return render_template("contact_me.html", title="Sign In", form=form)
+        return redirect(url_for("html_page", page_name="form_submitted"))
+    return render_template("contact_me.html", form=form)
 
 
 @app.route("/<string:page_name>")
 def html_page(page_name):
-    return render_template(page_name)
+    return render_template(page_name + ".html")
